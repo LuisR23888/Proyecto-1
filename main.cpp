9aa5313567373f9d85fb4dcc8a967adc0bba1cfa
LuisR23888 <rod23888@uvg.edu.gt>
2025-08-25 22:37:18 -0600

    Código final
    Para este código final se añadió la función de enviar información sobre la temperatura al conjunto de displays utilizando el contador interno del ESP32 debido a que el io.run interdiere con la implementación del multiplex. Con la implementación de la función de los displays, también se añade la librería donde se configuraron las funciones de la misma. Finalmente, se mejoró la interrupción del botón 1 y se cambiaron pines de algunos componentes para un mejor diseño físico.

--- main.cpp
+++ main.cpp
@@ -7,34 +7,39 @@
 // MCU: ESP32 dev kit 1.1
 //**********************************************************************/
 
-
+//******************************************/
+// Librerias
+//******************************************/
 #include <Arduino.h>
 #include <stdint.h>
 #include <driver/ledc.h>
 #include "config.h"
+#include "Display7.h"
 
-// Definciones
-#define ADCPIN 34
+//******************************************/
+// Definiciones
+//******************************************/
+#define display1 21
+#define display2 22
+#define display3 23
 
+#define ADCPIN 32
 #define servo 25
 
 #define LED_R 13
-#define LED_V 14
-#define LED_A 26
-#define BTN1 0
+#define LED_V 12
+#define LED_A 27
+#define BTN1 15
 
 #define pwmChannel3 3
-
 #define freqPWMS 50
-
-
 #define resPWM 16
 
 #define delayBounce 250
 
-
-// Funciones
-
+//******************************************/
+// Prototipos de funciones
+//******************************************/
 void initBoton(void);
 void IRAM_ATTR BTN1_ISR (void);
 
@@ -43,55 +48,78 @@ void initPWM(void);
 void getADCEMA(void);
 
 
+//******************************************/
 // Variables globales
-
+//******************************************/
 AdafruitIO_Feed *tempCanal = io.feed("proyecto1-temp");
 
-float Temperatura;
 float ValorT=0;
-
-
-//volatile uint32_t contador;
 volatile int32_t contadorS;
-
 
 volatile bool btn1Pressed;
 volatile uint32_t lastISRBtn1 = 0;
 
-float adcRawEMA = 0;
-float adcFiltrado = adcRawEMA;
-float alpha = 0.05;
+float temperatureC;
+float alpha = 0.3;
+float adcFiltered = 0.0;
+int adcRaw = 0;
 
-//ISRs - Rutinas de interrupcion
+int decenas = 0;
+int unidades = 0;
+int decimas = 0;
+
+int currentDisplay = 0;
+
+//******************************************/
+// ISRs Rutinas de Interrupcion
+//******************************************/
 void IRAM_ATTR BTN1_ISR (void){
   uint32_t tiempoActual = millis();
 
   if(tiempoActual - lastISRBtn1 > delayBounce){
     btn1Pressed = true;
-    ValorT=Temperatura;
-    if (ValorT<22.0){
-      contadorS = 0;
-    }
-    else if (ValorT>22.0 && ValorT<25.0)
-    {
-      contadorS = 1;
-    }
-    else if (ValorT>25.0){
-      contadorS = 2;
-    }
     lastISRBtn1 = tiempoActual;
   }
 }
 
-//Rutinas de interrupcion
+hw_timer_t * timer = NULL;
 
+void IRAM_ATTR onTimer() {
+  // apagar todos
+  digitalWrite(display1, LOW);
+  digitalWrite(display2, LOW);
+  digitalWrite(display3, LOW);
+
+  switch(currentDisplay) {
+    case 0:
+      digitalWrite(display1, HIGH);
+      desplegarNumero(decenas);
+      desplegarPunto(0);
+      break;
+    case 1:
+      digitalWrite(display2, HIGH);
+      desplegarNumero(unidades);
+      desplegarPunto(1);
+      break;
+    case 2:
+      digitalWrite(display3, HIGH);
+      desplegarNumero(decimas);
+      desplegarPunto(0);
+      break;
+  }
+  currentDisplay = (currentDisplay + 1) % 3;
+}
 
 void setup() {
-  // put your setup code here, to run 
-  
   Serial.begin(115200);
   initBoton();
+  configDisplay7();
 
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000, true);
+  timerAlarmEnable(timer);
+  
   // wait for serial monitor to open
   while(! Serial);
 
@@ -115,11 +143,19 @@ void setup() {
   pinMode(LED_R, OUTPUT);
   pinMode(LED_V, OUTPUT);
   pinMode(LED_A, OUTPUT);
+
+  pinMode(display1, OUTPUT);
+  pinMode(display2, OUTPUT);
+  pinMode(display3, OUTPUT);
   
   // Inicializar estado de LEDs
   digitalWrite(LED_R, LOW);
   digitalWrite(LED_V, LOW);
   digitalWrite(LED_A, LOW);
+
+  digitalWrite(display1, LOW);
+  digitalWrite(display2, LOW);
+  digitalWrite(display3, LOW);
 
   initPWM();
 }
@@ -128,17 +164,32 @@ void loop() {
   io.run();
   getADCEMA();
 
-  Serial.print("sending -> ");
-  Serial.println(ValorT);
-  tempCanal->save(ValorT);
+  unsigned long currentMillis = millis();
 
-  delay(3000);
+  int tempC = ValorT * 10;
+  decenas = tempC / 100;
+  unidades = (tempC / 10) % 10;
+  decimas = tempC % 10;
 
-  Temperatura = map(adcFiltrado, 0, 4095, 0, 30);
-  Serial.println(adcRawEMA);
-  Serial.println(Temperatura);
+  if (btn1Pressed) {
+    btn1Pressed = false;
+    ValorT = temperatureC;
 
+    Serial.print("sending -> ");
+    Serial.println(ValorT);
+    tempCanal->save(ValorT);
 
+    if (ValorT < 22.0) {
+    contadorS = 0;
+    }
+    else if (ValorT >= 22.0 && ValorT < 25.0) {
+      contadorS = 1;
+    }
+    else if (ValorT >= 25.0) {
+      contadorS = 2;
+    }
+  }
+  
   switch(contadorS){
     case 0:
     ledcWrite(pwmChannel3, 3277);
@@ -162,19 +213,25 @@ void loop() {
     break;
   }
 }
-
-
+//******************************************/
+// Otras funciones
+//******************************************/
 void initBoton(void){
   pinMode(BTN1, INPUT_PULLUP);
   attachInterrupt(BTN1, &BTN1_ISR, FALLING);
-
 }
 
 void initPWM(void) {
+    ledcSetup(pwmChannel3, freqPWMS, resPWM);
     ledcAttachPin(servo, pwmChannel3);
 }
 
 void getADCEMA(void){
-  adcRawEMA = analogRead(ADCPIN);
-  adcFiltrado = (alpha * adcRawEMA) + ((1.0-alpha)*adcFiltrado);
+  adcRaw = analogReadMilliVolts(ADCPIN);   
+  adcFiltered = alpha * adcRaw + (1 - alpha) * adcFiltered;  
+  
+  temperatureC = (adcFiltered / 10.0)-8;
+  
+  temperatureC = roundf(temperatureC * 10) / 10.0;
 }
+
